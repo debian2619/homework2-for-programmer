d0619a21599c549526851d8e6036d2a9fffcff5a
Dmitry <ya.potapov@yandex.ru>
2022-09-04 12:20:53 +0300

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿int Factorial(int n)
+{
+    if (n == 1) return 1;
+ 
+    return n * Factorial(n - 1);
+}
+ 
+int factorial4 = Factorial(4);  // 24
+int factorial5 = Factorial(5);  // 120
+int factorial6 = Factorial(6);  // 720
+ 
+Console.WriteLine($"Factorial 4 = {factorial4}");
+Console.WriteLine($"Factorial 5 = {factorial5}");
+Console.WriteLine($"Factorial 6 = {factorial6}");
