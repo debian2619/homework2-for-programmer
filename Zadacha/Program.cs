7b57c3b355dc551cb0b196bd93435875f5b738c2
Dmitry <ya.potapov@yandex.ru>
2022-09-04 12:36:17 +0300

    Zadacha

---
+++ Zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+string[] array =
+{
+    "hello",
+    "2",
+    "world",
+    ":-)",
+    
+    
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+
